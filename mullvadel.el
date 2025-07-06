0c0faa497611388bc6a0a7433cb186490d2daeb8
yelobat <yelobat2@gmail.com>
2025-07-06 04:16:06 +0100

    updated mullvadel.el to implement more commands.

--- mullvadel.el
+++ mullvadel.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: transient comm tools unix processes
 ;; Homepage: https://github.com/yelobat/mullvadel
-;; Package-Requires: ((emacs "28.1") (transient "0.9.2"))
+;; Package-Requires: ((emacs "28.1") (transient "0.9.2") (compat))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -55,21 +55,75 @@
        (message "Error running Mullvad VPN CLI command '%s': %S" command err)
        (error "Mullvad CLI command failed: %S" (cdr err))))))
 
+;; Mullvad VPN commands
+(defun mullvadel-status ()
+  "Return the state of the VPN tunnel."
+  (interactive)
+  (mullvadel-cli-command-run '("status")))
+
+;; Mullvad VPN account commands
 (defun mullvadel-account-create ()
   "Create and log in on a new account."
+  (interactive)
   (mullvadel-cli-command-run '("account" "create")))
 
 (defun mullvadel-account-login ()
   "Log in on an account."
+  (interactive)
   (mullvadel-cli-command-run '("account" "login")))
 
 (defun mullvadel-account-logout ()
   "Log out of the current account."
+  (interactive)
   (mullvadel-cli-command-run '("account" "logout")))
 
 (defun mullvadel-account-get ()
   "Display information about the current account."
+  (interactive)
   (mullvadel-cli-command-run '("account" "get")))
+
+;; Mullvad VPN auto-connect commands
+(defun mullvadel-auto-connect-get ()
+  "Display the current auto-connect setting."
+  (interactive)
+  (mullvadel-cli-command-run '("auto-connect" "get")))
+
+(defun mullvadel-auto-connect-set ()
+  "Change auto-connect setting."
+  (interactive)
+  (mullvadel-cli-command-run '("auto-connect" "set")))
+
+;; Mullvad VPN beta-program commands
+(defun mullvadel-beta-program-get ()
+  "Get beta notifications setting."
+  (interactive)
+  (mullvadel-cli-command-run '("beta-program" "get")))
+
+(defun mullvadel-beta-program-set ()
+  "Change beta notifications setting."
+  (interactive)
+  (mullvadel-cli-command-run '("beta-program" "set")))
+
+(defun mullvadel-connectedp ()
+  "Return t if the connected state of the VPN tunnel is \"Connected\"."
+  (let ((output (mullvadel-status)))
+    (if (equal (car (string-split output "\n")) "Connected")
+        t
+        nil)))
+
+(transient-define-prefix mullvadel-menu ()
+  "Mullvad VPN commands."
+  ["Mullvad VPN commands"
+   ["General" :description "Display general information."
+    ("s" "status" mullvadel-status :description "Return the state of the VPN tunnel.")]
+   ["account" :description "Control and display information about your Mullvad account."
+    ("ac" "create" mullvadel-account-create :description "Create and log in on a new account.")
+    ("ai" "login" mullvadel-account-login :description "Log in on an account.")
+    ("ao" "logout" mullvadel-account-logout :description "Log out of the current account.")
+    ("ag" "get" mullvadel-account-get :description "Display information about the current account.")]
+   ["auto-connect" :description "Control the daemon auto-connect setting."
+    ("ug" "get" mullvadel-auto-connect-get :description "Display the current auto-connect setting.")
+    ("us" "set" mullvadel-auto-connect-set :description "Change auto-connect setting.")]])
 
 (provide 'mullvadel)
 ;;; mullvadel.el ends here
